c57d3abeddee0c20a04a6c2d8888af1d80b39c7f
amsg18 <amsg18@alu.ua.es>
2022-05-02 13:49:26 +0200

    Aplicación ToDo - Autenticación III: User Data

--- angular/to-do-app/src/app/app.component.ts
+++ angular/to-do-app/src/app/app.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toDoApp para SD';
 
   email: string= 'test';
   password: string= 'test';
+
+  displayName='';
+
   constructor(public userService: UserService){}
 
   ngOnInit(){
     this.login();
+    this.userService.getUser().subscribe(
+      user => {this.displayName = user.nombre
+        console.log(user);
+      }); 
   }
+
   login(){
     const user={email:this.email, password: this.password};
+    
     this.userService.signIn(user).subscribe(
       (loggedUser)=> {
         this.userService.setToken(loggedUser.token);
