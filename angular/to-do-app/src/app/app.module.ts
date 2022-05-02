d6d008662c776acafffb42b3c4ac255f7facc5b7
amsg18 <amsg18@alu.ua.es>
2022-05-02 11:18:26 +0200

    Aplicación ToDo - Primera versión sin autenticación

---
+++ angular/to-do-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
+import  {FormsModule} from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
